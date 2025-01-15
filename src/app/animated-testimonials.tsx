e66e79d75af480143ca58b0625cfc04643a7979a
syed <syed@kemuri.in>
2025-01-15 08:15:19 +0530

    shorter description

--- src/app/animated-testimonials.tsx
+++ src/app/animated-testimonials.tsx
@@ -3,15 +3,13 @@ import { AnimatedTestimonials } from "@/components/ui/animated-testimonials";
 export function AnimatedTestimonialsDemo() {
   const testimonials = [
     {
-      quote:
-        "React Native app, similar to BookMyShow, offering detailed navigation to event halls or locations using AR vision for a more immersive & interactive experience",
+      quote: "React Native app, similar to BookMyShow",
       name: "MaSeat?",
       designation: "PlayStore | AppStore",
       src: "/app3.webp",
     },
     {
-      quote:
-        "An OLLMA 3.2-powered education app that predicts recurring questions based on patterns from previous exams",
+      quote: "An OLLMA 3.2-powered education app",
       name: "Failerrrr",
       designation: "Devloper Mode",
       src: "/app1.webp",
