0ac4262b893817a70a63f25b7642c803bda04e45
syed <syed@kemuri.in>
2025-04-14 13:55:47 +0530

    fixed png issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -253,58 +253,12 @@ export default function Page() {
             <AnimatedTestimonialsDemo></AnimatedTestimonialsDemo>
           </BlurFade>
         </section>
-        <section id="hackathons">
-          <div className="space-y-12 w-full py-12">
-            <BlurFade delay={BLUR_FADE_DELAY * 13}>
-              <div className="flex flex-col items-center justify-center space-y-4 text-center">
-                <div className="space-y-2">
-                 <img
-                    src="/decor/iss.png"
-                    alt="Space Station"
-                    className="w-full my-4 md:my-6  lg:my-12"
-                  />
-                  <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                    Projects & Patents
-                  </div>
-                  <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                    Ideas & Innovations
-                  </h2>
-                 
-                  <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                    Each piece allowed me to explore and articulate my
-                    understanding of complex topics, fostering a deeper
-                    appreciation for the intersection of theory & practice.
-                  </p>
-                </div>
-              </div>
-            </BlurFade>
-            <BlurFade delay={BLUR_FADE_DELAY * 14}>
-              <ul className="mb-4 ml-4 divide-y divide-dashed border-l">
-                {DATA.hackathons.map((project, id) => (
-                  <BlurFade
-                    key={project.title + project.dates}
-                    delay={BLUR_FADE_DELAY * 15 + id * 0.05}
-                  >
-                    <HackathonCard
-                      title={project.title}
-                      description={project.description}
-                      location={project.location}
-                      dates={project.dates}
-                      image={project.image}
-                      links={project.links}
-                    />
-                  </BlurFade>
-                ))}
-              </ul>
-            </BlurFade>
-          </div>
-        </section>
         <section id="contact">
           <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
             <BlurFade delay={BLUR_FADE_DELAY * 16}>
               <div className="space-y-3">
                 <img
-                    src="/decor/bike.png"
+                    src="/decor/Bike.png"
                     alt="Bike Outline"
                     className="w-full"
                   />
