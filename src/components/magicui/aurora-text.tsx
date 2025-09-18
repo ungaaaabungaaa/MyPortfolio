88209eda795cb657d98833f840e34e40832a6fe1
syed abdul muqeeth <samx123786@gmail.com>
2025-09-19 02:15:57 +0530

    style: update padding in layout and modify color gradient in AuroraText component for improved aesthetics

--- src/components/magicui/aurora-text.tsx
+++ src/components/magicui/aurora-text.tsx
@@ -13,7 +13,7 @@ export const AuroraText = memo(
   ({
     children,
     className = "",
-    colors = ["#FF0080", "#7928CA", "#0070F3", "#38bdf8"],
+    colors = ["#00F260","#0575E6"],
     speed = 1,
   }: AuroraTextProps) => {
     const gradientStyle = {
